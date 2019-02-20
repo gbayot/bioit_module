35c3141a74af5444ca48463cd117a5d77974ffe3
Maxime Lienard <maxime.lienard@oncodna.com>
2019-02-20 11:20:20 +0100

    Add first set of unit test

--- bioit_module/base_module.py
+++ bioit_module/base_module.py
@@ -11,228 +11,111 @@ from optparse import OptionParser
 
 class BaseModule(abc.ABC):
     def __init__(self, name, version, suffix, install_config=None, no_parameters=False, usage=None):
-        self.__NAME__ = name
-        self.__VERSION__ = version
-        self.__install_config_file__ = install_config
-        self.__suffix__ = suffix
-        self.__no_parameters__ = no_parameters
-        self.__set_usage_str__(usage)
-        self.__parser__ = None
-        self.__outdir__ = None
-        self.__prefix__ = None
-        self.__job__ = None
-        self.__run__ = None
-        self.__install_config__ = None
-        self.__parameters__ = None
-        self.__param_file__ = None
-        self.__json_log__ = None
+        self.name = name
+        self.version = version
+        self._install_config_file = install_config
+        self.suffix = suffix
+        self._no_parameters = no_parameters
+        self._set_usage_str(usage)
+        self.parser = None
+        self.output_dir = None
+        self.prefix = None
+        self.job_id = None
+        self.run_id = None
+        self.install_config = None
+        self.parameters = None
+        self._param_file = None
+        self._json_log = None
 
-    def __set_usage_str__(self, usage=None):
+    def _set_usage_str(self, usage=None):
         if usage is not None:
-            self.__usage_str__ = usage
+            self._usage_str = usage
         else:
-            self.__usage_str__ = "Usage: %prog [options] -o prefix"
-            if not self.__no_parameters__:
-                self.__usage_str__ += " -p parameters.ini"
-            self.__usage_str__ += " input"
+            self._usage_str = "Usage: %prog [options] -o prefix"
+            if not self._no_parameters:
+                self._usage_str += " -p parameters.ini"
+            self._usage_str += " input"
         return
 
-    @property
-    def parser(self):
-        return self.__parser__
+    def init_std_options(self):
+        self.parser = OptionParser(usage=self._usage_str)
+        self.parser.version = self.version
+        self.parser.add_option("-o", "--out-prefix", dest="prefix", metavar="Prefix",
+                               help="Prefix use to name output files (ex: output_dir/sample_1). "
+                                    "If it doesn't exist, output directory will be created.")
+        if not self._no_parameters:
+            self.parser.add_option("-p", "--params", dest="param", help="Module parameters file.",
+                                   metavar="FILE")
+        if self._install_config_file is not None:
+            self.parser.add_option("-c", "--config", dest="config",
+                                   default=self._install_config_file, metavar="FILE",
+                                   help="Use a specific install configuration file.")
+        self.parser.add_option("-j", "--job", dest="jobid", default="0",
+                               help="Unique job identifier", metavar="STRING")
+        self.parser.add_option("-r", "--run", dest="runid", default="0",
+                               help="Unique run identifier", metavar="STRING")
+        self.parser.add_option("-l", "--log", dest="logfile",
+                               help="Alignment execution log file, STDOUT if not defined.",
+                               metavar="STRING")
+        self.parser.add_option("-d", "--debug", dest="debug", default=False, action="store_true",
+                               metavar="BOOLEAN", help="Set log level to debug.")
+        self.parser.add_option("-v", "--version", dest="version", help="Print version and exit",
+                               action="store_true", default=False, metavar="INT")
 
-    @property
-    def config(self):
-        return self.__install_config__
-
-    @property
-    def parameters(self):
-        return self.__parameters__
-
-    @property
-    def output_dir(self):
-        return self.__outdir__
-
-    @property
-    def suffix(self):
-        return self.__suffix__
-
-    @property
-    def prefix(self):
-        return self.__prefix__
-
-    @property
-    def name(self):
-        return self.__NAME__
-
-    @property
-    def version(self):
-        return self.__VERSION__
-
-    def run(self, **kwargs):
-        # Initialisation of standard bioit module options.
-        self.init_std_options(inst_config=self.__install_config_file__, usage_str=self.__usage_str__)
-        # Initialisation of custom bioit module options.
-        self.init_custom_options()
-        # Read options and arguments.
-        (options, args) = self.__parser__.parse_args()
-        # Verifications of standard bioit module options.
-        self.check_std_options(options)
-        # Verifications of custom bioit module options.
-        self.check_custom_options(options)
-        # Check  input arguments.
-        self.check_args(args)
-
-        # ---------- After this step logging system is initialized ---------- #
-
-        # Initialized output files names and create directory if needed
-        logging.debug("Check input arguments.")
-        self.__outdir__ = "/".join(options.prefix.split("/")[:-1])
-        if self.__outdir__ == "":
-            self.__outdir__ = "."
-        logging.debug("Output directory name: %s", self.__outdir__)
-        self.__prefix__ = options.prefix.split("/")[-1]
-        logging.debug("Output file prefix: %s", self.__prefix__)
-        self.__json_log__ = os.path.join(self.__outdir__, self.__prefix__ + "_mod_log.json")
-        logging.debug("Execution summary json file: %s", self.__json_log__)
-        if not os.path.isdir(self.__outdir__):
-            logging.info("Create non-existing output directories %s", self.__outdir__)
-            os.makedirs(self.__outdir__)
-        if os.path.isfile(self.__json_log__):
-            logging.warning("Execution summary json file: %s exist and will be replaced", self.__json_log__)
-
-        # load install config
-        if self.__install_config_file__ is not None:
-            try:
-                logging.debug("Loading install configuration file: %s", self.__install_config_file__)
-                self.__install_config__ = ConfigParser()
-                self.__install_config__.read(self.__install_config_file__)
-                # check config
-                logging.debug("Check install configuration file: %s", self.__install_config_file__)
-                self.check_install_config(self.__install_config__)
-            except Exception as e:
-                self.end_process(error="Unexpected error while loading install configuration file %s: %s" %
-                                       (self.__install_config__, str(e)), error_code=10)
-
-        # load parameter file (only if needed)
-        if not self.__no_parameters__:
-            try:
-                logging.debug("Loading parameter file: %s", self.__param_file__)
-                self.__parameters__ = ConfigParser()
-                self.__parameters__.read(self.__param_file__)
-                # check config
-                logging.debug("Check parameter file: %s", self.__param_file__)
-                self.check_parameters(self.__parameters__)
-            except Exception as e:
-                self.end_process(error="Unexpected error while loading parameters file %s: %s" %
-                                       (self.__param_file__, str(e)), error_code=11)
-        else:
-            logging.debug("Module without parameters.")
-
-        # start module process
-        try:
-            output_files_list = self.start_process(**kwargs)
-            self.end_process(error_code=0, files=output_files_list)
-        except NotImplementedError as nie:
-            raise nie
-        except Exception as e:
-            self.end_process(error_code=911, error="Unexpected error: %s:\n%s" % (str(e), traceback.format_exc()))
-
-    def end_process(self, error_code=0, error=None, files=None):
-        # if process end with error, print info in log system
-        if error_code != 0 and error is not None:
-            logging.error(error)
-        # Print analysis execution summary in JSON format.
-        if self.__json_log__ is None:
-            file_handler = sys.stderr
-        else:
-            file_handler = open(self.__json_log__, "w")
-        if files is None:
-            files = []
-        if error is None:
-            error = ""
-        file_handler.write(json.dumps(
-            dict({
-                "meta": dict({
-                    "status": error_code,
-                    "jobid": self.__job__,
-                    "runid": self.__run__,
-                    "error": error,
-                    "tool": self.__NAME__,
-                    "version": self.__VERSION__
-                }),
-                "files": files
-            })
-        ))
-        if file_handler == sys.stderr:
-            file_handler.write("\n")
-        file_handler.close()
-
-        # Exit script.
-        exit(error_code)
-
-    def init_std_options(self, usage_str=None, inst_config=None):
-        if usage_str is None:
-            usage_str = "Usage: %prog [options] -o prefix input"
-        self.__parser__ = OptionParser(usage=usage_str)
-        self.__parser__.version = self.__VERSION__
-        self.__parser__.add_option("-o", "--out-prefix", dest="prefix", metavar="Prefix",
-                                   help="Prefix use to name output files (ex: output_dir/sample_1). "
-                                        "If it doesn't exist, output directory will be created.")
-        if not self.__no_parameters__:
-            self.__parser__.add_option("-p", "--params", dest="param", help="Module parameters file.", metavar="FILE")
-        if inst_config is not None:
-            self.__parser__.add_option("-c", "--config", dest="config", default=inst_config, metavar="FILE",
-                                       help="Use a specific install configuration file.")
-        self.__parser__.add_option("-j", "--job", dest="jobid", default="0",
-                                   help="Unique job identifier", metavar="STRING")
-        self.__parser__.add_option("-r", "--run", dest="runid", default="0",
-                                   help="Unique run identifier", metavar="STRING")
-        self.__parser__.add_option("-l", "--log", dest="logfile",
-                                   help="Alignment execution log file, STDOUT if not defined.", metavar="STRING")
-        self.__parser__.add_option("-d", "--debug", dest="debug", default=False, action="store_true",
-                                   metavar="BOOLEAN", help="Set log level to debug.")
-        self.__parser__.add_option("-v", "--version", dest="version", help="Print version and exit",
-                                   action="store_true", default=False, metavar="INT")
-        return
+    def init_custom_options(self):
+        pass
 
     def check_std_options(self, options):
         # if version is required print and exit
         if options.version:
-            print(os.path.basename(sys.argv[0]) + " version " + self.__VERSION__)
-            exit(0)
+            print(os.path.basename(sys.argv[0]), "version", self.version)
+            return
 
         # init log system
         log_level = logging.INFO
         if options.debug:
             log_level = logging.DEBUG
-        format_str = '['+str(self.__NAME__)+'][%(levelname)s]:%(asctime)s:%(message)s'
+        format_str = '[' + str(self.name) + '][%(levelname)s]:%(asctime)s:%(message)s'
         format_date = '%m/%d/%Y %H:%M:%S'
         if options.logfile:
             logging.basicConfig(format=format_str, filename=options.logfile, level=log_level, datefmt=format_date)
         else:
             logging.basicConfig(format=format_str, level=log_level, datefmt=format_date)
 
+        # ---------- After this step logging system is initialized ---------- #
+
+        logging.debug("Check input arguments.")
+
         # set run and job
-        self.__run__ = str(options.runid)
-        self.__job__ = str(options.jobid)
+        self.run_id = str(options.runid)
+        self.job_id = str(options.jobid)
 
         # check prefix options and set output base
-        reg = re.compile('[^\w\s\/\.@_\-+~$*=]')
+        reg = re.compile(r'[^\w\s/\.@_\-+~$*=]')
         if options.prefix is None:
-            self.__parser__.error("Mandatory option -o prefix is missing")
+            self.parser.error("Mandatory option -o prefix is missing")
         elif re.search(reg, options.prefix):
-            self.__parser__.error("Invalid mandatory option -o prefix '" + str(options.prefix) +
-                                  "' contains illegal characters.")
-        # set config file
-        if self.__install_config_file__ is not None:
-            self.__install_config_file__ = options.config
-        if not self.__no_parameters__:
-            # set parameter file
-            self.__param_file__ = options.param
+            self.parser.error("Invalid mandatory option -o prefix '%s' contains illegal characters." %
+                              options.prefix)
 
-    def init_custom_options(self):
-        pass
+        output_prefix = os.path.split(options.prefix)
+        self.output_dir = os.path.sep.join(output_prefix[:-1])
+        if self.output_dir == "":
+            self.output_dir = "."
+
+        logging.debug("Output directory name: %s", self.output_dir)
+        self.prefix = output_prefix[-1]
+        logging.debug("Output file prefix: %s", self.prefix)
+        self._json_log = os.path.join(self.output_dir, self.prefix + "_mod_log.json")
+        logging.debug("Execution summary json file: %s", self._json_log)
+
+        # set config file
+        if self._install_config_file is not None:
+            self._install_config_file = options.config
+
+        # set parameter file
+        if not self._no_parameters:
+            self._param_file = options.param
 
     def check_custom_options(self, options):
         pass
@@ -240,11 +123,114 @@ class BaseModule(abc.ABC):
     def check_args(self, args):
         pass
 
+    def init_output(self):
+        # Initialized output files names and create directory if needed
+        if not os.path.isdir(self.output_dir):
+            logging.info("Create non-existing output directories %s", self.output_dir)
+            os.makedirs(self.output_dir)
+        if os.path.isfile(self._json_log):
+            logging.warning("Execution summary json file: "
+                            "%s exist and will be replaced", self._json_log)
+
+    def load_install_config(self):
+        if self._install_config_file is not None:
+            try:
+                # TODO: Add file exist test
+                logging.debug("Loading install configuration file: %s",
+                              self._install_config_file)
+                self.install_config = ConfigParser()
+                self.install_config.read(self._install_config_file)
+                # check config
+                logging.debug("Check install configuration file: %s",
+                              self._install_config_file)
+                self.check_install_config(self.install_config)
+            except Exception as e:
+                return self.end_process(
+                    error="Unexpected error while loading install configuration file "
+                          "%s: %s\n%s" %
+                          (self._install_config_file,  str(e), traceback.format_exc()),
+                    error_code=10)
+        else:
+            logging.debug("Module without specific installation configuration.")
+        return 0
+
     def check_install_config(self, install_config):
         pass
 
+    def load_parameter_file(self):
+        # load parameter file (only if needed)
+        if not self._no_parameters:
+            try:
+                logging.debug("Loading parameter file: %s", self._param_file)
+                self.parameters = ConfigParser()
+                self.parameters.read(self._param_file)
+                # check config
+                logging.debug("Check parameter file: %s", self._param_file)
+                self.check_parameters(self.parameters)
+            except Exception as e:
+                return self.end_process(
+                    error="Unexpected error while loading parameters file %s: %s\n%s" %
+                          (self._param_file,  str(e), traceback.format_exc()),
+                    error_code=11)
+        else:
+            logging.debug("Module without parameters.")
+        return 0
+
     def check_parameters(self, parameters):
         pass
+
+    def run(self, **kwargs):
+        ec = 0
+        # Initialisation of standard bioit module options.
+        self.init_std_options()
+        # Initialisation of custom bioit module options.
+        self.init_custom_options()
+        # Read options and arguments.
+        (options, args) = self.parser.parse_args()
+        # Verifications of standard bioit module options.
+        self.check_std_options(options)
+        # Verifications of custom bioit module options.
+        self.check_custom_options(options)
+        # Check  input arguments.
+        self.check_args(args)
+        # Initialized output files names and create directory if needed
+        self.init_output()
+        # load install config
+        if ec == 0:
+            ec = self.load_install_config()
+        # load parameter file
+        if ec == 0:
+            ec = self.load_parameter_file()
+        # start module process
+        if ec == 0:
+            try:
+                output_files_list = self.start_process(**kwargs)
+                ec = self.end_process(error_code=0, files=output_files_list)
+            except NotImplementedError as nie:
+                raise nie
+            except Exception as e:
+                ec = self.end_process(error="Unexpected error: %s:\n%s" %
+                                            (str(e), traceback.format_exc()), error_code=911)
+        return ec
+
+    def end_process(self, error_code=0, error="", files=[]):
+        # if process end with error, print info in log system
+        if error_code != 0 and error is not None:
+            logging.error(error)
+        # Print analysis execution summary in JSON format.
+        with open(self._json_log, "w") as fp:
+            json.dump({
+                "meta": {
+                    "status": error_code,
+                    "jobid": self.job_id,
+                    "runid": self.run_id,
+                    "error": error,
+                    "tool": self.name,
+                    "version": self.version
+                },
+                "files": files
+            }, fp)
+        return error_code
 
     @abc.abstractmethod
     def start_process(self, **kwargs):
---
+++ tests/test_base_module.py
@@ -0,0 +1,137 @@
+import shutil
+from unittest import mock
+import pytest, os
+from bioit_module.base_module import BaseModule
+
+@pytest.fixture
+def opts():
+    options = mock.Mock()
+    options.version = False
+    options.runid = "01"
+    options.jobid = "02"
+    options.prefix = "this/is/a/test"
+    return options
+
+
+@pytest.fixture
+def module():
+    return FakeModule("name", "version", "suffix")
+
+
+class FakeModule(BaseModule):
+    def start_process(self, **kwargs):
+        pass
+
+
+def test_cannot_instantiate():
+    with pytest.raises(TypeError):
+        BaseModule()
+
+
+def test_init_module_require_args():
+    with pytest.raises(TypeError):
+        mod = FakeModule()
+
+
+def test_init_module():
+    mod = FakeModule("name", "version", "suffix")
+    assert mod.name == "name"
+    assert mod.version == "version"
+    assert mod.suffix == "suffix"
+
+
+def test_init_module_with_usage():
+    mod = FakeModule("name", "version", "suffix", usage="RTFM")
+    mod.init_std_options()
+    assert mod.parser.usage == "RTFM"
+
+
+def test_init_module_with_config():
+    mod = FakeModule("name", "version", "suffix", install_config="test.ini")
+    mod.init_std_options()
+    assert mod.name == "name"
+    assert mod.version == "version"
+    assert mod.suffix == "suffix"
+    assert mod._install_config_file == "test.ini"
+
+
+def test_init_module_with_parameters():
+    mod = FakeModule("name", "version", "suffix", no_parameters=True)
+    mod.init_std_options()
+    assert mod._no_parameters == True
+
+
+def test_check_std_options_no_prefix(opts, module):
+    opts.prefix = None
+    module.init_std_options()
+    with pytest.raises(SystemExit):
+        module.check_std_options(options=opts)
+
+
+def test_check_std_options_invalid_prefix(opts, module):
+    opts.prefix = "some^invalids?chars%inùpath"
+    module.init_std_options()
+    with pytest.raises(SystemExit):
+        module.check_std_options(options=opts)
+
+
+def test_check_std_options_pass(opts, module):
+    module.check_std_options(options=opts)
+    assert module.run_id == "01"
+    assert module.job_id == "02"
+    assert module.output_dir == "this/is/a"
+    assert module.prefix == "test"
+    assert module.version == "version"
+
+
+@mock.patch('optparse.OptionParser.parse_args')
+def test_run_noparam_noconfig(mock_parse_args, opts):
+    mod = FakeModule("name", "version", "suffix", no_parameters=True)
+    args = ["not an input file"]
+    mock_parse_args.return_value = (opts, args)
+    assert mod.run() == 0
+    assert os.path.isfile(os.path.join(mod.output_dir, mod.prefix + "_mod_log.json"))
+    # remove created output directory
+    shutil.rmtree("this")
+
+
+def test_load_config_no_file_error():
+    mod = FakeModule("name", "version", "suffix")
+    mod._install_config_file = "NotAFile"
+    assert mod.load_install_config() == 0
+
+
+@mock.patch('bioit_module.BaseModule.check_install_config')
+def test_load_config_file_check_error(mock_check_install_config):
+    mod = FakeModule("name", "version", "suffix")
+    mod._install_config_file = "NotAFile"
+    mod._json_log = "summary.json"
+    mock_check_install_config.side_effect = Exception("Random error")
+    assert mod.load_install_config() == 10
+    os.unlink("summary.json")
+
+
+@mock.patch('bioit_module.BaseModule.check_install_config')
+def test_load_config_file_check_ok(mock_check_install_config):
+    mod = FakeModule("name", "version", "suffix")
+    mod._install_config_file = "pytest.ini"
+    mock_check_install_config.return_value = None
+    assert mod.load_install_config() == 0
+
+
+@mock.patch('bioit_module.BaseModule.check_parameters')
+def test_load_parameter_file_check_error(mock_check_parameters):
+    mod = FakeModule("name", "version", "suffix")
+    mod._param_file = "NotAFile"
+    mod._json_log = "summary.json"
+    mock_check_parameters.side_effect = Exception("Random error")
+    assert mod.load_parameter_file() == 11
+    os.unlink("summary.json")
+
+
+@mock.patch('bioit_module.BaseModule.check_parameters')
+def test_load_config_file_check_ok(mock_check_parameters):
+    mod = FakeModule("name", "version", "suffix")
+    mod._param_file = "pytest.ini"
+    mock_check_parameters.return_value = None
+    assert mod.load_parameter_file() == 0
