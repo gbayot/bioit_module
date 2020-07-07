1c4b0fb486c93c03e3c7cf392262d4cf715f272c
Guillaume Bayot <gb@oncodna.com>
2020-07-07 11:05:47 +0200

    default exit code

--- bioit_module/exit_code.py
+++ bioit_module/exit_code.py
@@ -1,6 +1,9 @@
+UnknownError = 1
 ValidationArgsError = 3
 NoOptionError = 4
 ValidationError = 5
 ValidationInstallConfigError = 6
 ValidationParamsError = 7
-UnknownError = 911
+SigKillOOM = 137
+SigSegv = 139
+SigTerm = 143
